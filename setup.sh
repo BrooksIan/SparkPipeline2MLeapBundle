a992d424899976d102d296e1b9f936fd80474b50
Ian Brooks <brooks_ian@yahoo.com>
2021-03-24 13:53:36 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,11 @@ pip install jip
 pip3 install jip
 pip install mleap
 pip3 install mleap
+
+wget https://repo1.maven.org/maven2/ml/bundle/bundle-ml_2.11/0.1.1/bundle-ml_2.11-0.1.1.jar -O /etc/spark/jars/bundle-ml.jar
+wget https://repo1.maven.org/maven2/ml/combust/mleap/mleap-spark-base_2.11/0.16.0/mleap-spark-base_2.11-0.16.0.jar -O /etc/spark/jars/mleap-spark-base.jar 
+wget https://repo1.maven.org/maven2/ml/combust/mleap/mleap-base_2.12/0.16.0/mleap-base_2.12-0.16.0.jar -O /etc/spark/jars/mleap-base.jar
+wget https://repo1.maven.org/maven2/ml/combust/mleap/mleap-core_2.12/0.16.0/mleap-core_2.12-0.16.0.jar -O /etc/spark/jars/mleap-core.jar 
+wget https://repo1.maven.org/maven2/ml/combust/mleap/mleap-executor_2.12/0.16.0/mleap-executor_2.12-0.16.0.jar -O /etc/spark/jars/mleap-executor.jar 
+wget https://repo1.maven.org/maven2/ml/combust/mleap/mleap-spark-extension_2.12/0.16.0/mleap-spark-extension_2.12-0.16.0.jar -O /etc/spark/jars/mleap-spark-extension.jar 
+wget https://repo1.maven.org/maven2/ml/combust/mleap/mleap-spark_2.12/0.16.0/mleap-spark_2.12-0.16.0.jar -O /etc/spark/jars/mleap-spark.jar
